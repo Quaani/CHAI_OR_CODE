8cf350bcbf6abe9c72fbf9aa17d9de956003d460
Quaani <onemusicless@gmail.com>
2024-12-05 09:49:59 +0000

    continue

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat as new verson of js
+
+// alert(3+3) // we are using node js, not browser
+
+console.log(3+3) //making good looking project
+
+console.log("bhuwan")
+
+let name="bhuwan"
+let age=19
+let isLoggedIn=false
+let state; null
+
+// number =>2 to power 53
+// bigint = big numbers used on like trading
+// string=> ""
+// boolean=>true/false
+// null => standalone value
+// undefined =>
+// symbol=>findout uniqueness
+
+
+// boject
+
+console.log(typeof age);
