a264fc701c8eddfe52e7054cd5f32f32c8e07add
Quaani <onemusicless@gmail.com>
2024-12-05 09:16:51 +0000

    continue

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 124432
+let accountEmail = "bhuwanxettri4@gmail.com"
+var accountPassword = "12345"
+accountCity = "nepal"
+let accountState;
+
+// accountId = 2 //not allowed
+accountEmail = "bhuwanHC.com"
+accountPassword = "0102036"
+accountCity = "india"
+
+/*
+perfer not to use var
+*/
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
