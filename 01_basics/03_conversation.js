4857040c74352aa97f027ffb0f2c37561c1d886b
Quaani <onemusicless@gmail.com>
2024-12-06 08:05:47 +0000

    conversation

---
+++ 01_basics/03_conversation.js
@@ -0,0 +1,30 @@
+let score="34asd"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// null = 0 (object)
+// undefined=same{NaN}
+// true=bullean value {1}
+
+// "22"=>22
+// "34asd"=>NaN{Not a Number}
+// true=>1; false=>0
+
+let isLoggedIn = ""
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
+
+// 1=>true; ""=>false; 0=>false; "value"=>true
+
+let someNumber = 22
+
+// let stringNumber = String(someNumber)
+// console.log( stringNumber);
+
+
